6c423e2efe422c0afe5cdcc8954666bb2fb375cd
heytherepeter <jaretep@gmail.com>
2020-04-14 03:53:17 +0000

    added descriptions

--- variable.tf
+++ variable.tf
@@ -1,7 +1,13 @@
 variable "sdm_port" {
+  description = "Listening port advertised by LoadBalancer"
+  type = number
   default = 5000
 }
 variable "sdm_app_name" {
+  description = "App name used by Kubernetes to sync services"
+  type = string
 }
 variable "sdm_gateway_name" {
+  description = "Logical name for the strongDM gateway"
+  type = string
 }
